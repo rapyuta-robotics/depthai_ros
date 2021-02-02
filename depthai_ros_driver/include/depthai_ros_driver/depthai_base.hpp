098784efda407b51615ed692974f2b0c8da757da
Kunal Tyagi <tyagi.kunal@live.com>
2021-02-02 11:43:38 +0900

    Adding ability to set params for custom configurations

--- depthai_ros_driver/include/depthai_ros_driver/depthai_base.hpp
+++ depthai_ros_driver/include/depthai_ros_driver/depthai_base.hpp
@@ -87,6 +87,7 @@ private:
 
     // Parameters
     std::string _camera_name = "";
+    std::string _camera_param_uri = "package://depthai_ros_driver/params/camera/";
     std::string _cmd_file = "";
     std::string _calib_file = "";
     std::string _blob_file = "";
