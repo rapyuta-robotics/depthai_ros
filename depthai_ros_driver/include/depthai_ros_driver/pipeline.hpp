82d242177b9e32644a85d25d94fbd7805ee27766
Kunal Tyagi <tyagi.kunal@live.com>
2021-02-13 17:14:05 +0900

    Getting more of the pipeline class implemented

--- depthai_ros_driver/include/depthai_ros_driver/pipeline.hpp
+++ depthai_ros_driver/include/depthai_ros_driver/pipeline.hpp
@@ -1,31 +1,80 @@
+#pragma once
+
+#include <memory>
 #include <vector>
 
 #include <depthai/pipeline/Node.hpp>
 #include <depthai/pipeline/Pipeline.hpp>
 
 namespace rr {
-struct Pipeline {
-    using NodeConstPtr = std::shared<const dai::Node>;
+class Pipeline {
+protected:
+    dai::Pipeline _pipeline;
+
+public:
+    using NodeConstPtr = std::shared_ptr<const dai::Node>;
+
+    [[nodiscard]] std::vector<Pipeline::NodeConstPtr> getNodesByName(const std::vector<std::string>& node_names);
 
     /**
      * @brief The first function to get called, should have the setup ready for any future calls
+     *
      */
-    virtual void configure = 0;
+    void configure() { onConfigure(); }
 
     /**
-     * @brief interface called by the driver to get a ready pipeline
+     * @brief interface called by the driver to get a pipeline ready-to-go
+     *
+     * @return dai::Pipeline
      */
-    virtual dai::Pipeline get_pipeline() = 0;
+    dai::Pipeline getPipeline() const { return _pipeline; }
 
-    // TODO: Do these need to be virtual? Can it be implemented independently?
     /**
      * @brief returns the nodes that access the raw camera data. Not used for ROS interface
+     * @details uses names provided by `CameraNodeNames` to filter for the camera nodes
+     * @return std::vector<Pipeline::NodeConstPtr> list of camera nodes
      */
-    virtual std::vector<NodeConstPtr> getCameras() = 0;
+    std::vector<NodeConstPtr> getCameras() const { return filterNodesByName(getCameraNodeNames()); }
+
+protected:
+    /**
+     * @brief Default implementation for configure step, does nothing
+     */
+    virtual void onConfigure() {}
+
+    /**
+     * @brief Returns a pipeline in constant time
+     */
+    virtual dai::Pipeline onGetPipeline() const = 0;
+
+    /**
+     * @brief Get the list of names used for different camera node types
+     * @note Please note that the list is of node types, not the names provided to the individual nodes
+     * @details By default, it provides the 2 cameras in depthai-core: "{Mono,Color}Camera"
+     * The names should be the same as the return value of `dai::Node::getName()`
+     * @return std::vector<std::string> vector of node types
+     */
+    virtual std::vector<std::string> getCameraNodeNames() const { return {"MonoCamera", "ColorCamera"}; }
+
+    /////////////////////
+    std::vector<NodeConstPtr> getRawOutputs() const { return filterNodesByName({"XLinkOut"}); }
+    std::vector<NodeConstPtr> getRawInputs() const { return filterNodesByName({"XLinkIn"}); }
+
     /**
      * @brief returns the raw streams exposed by the ROS interface
      */
-    virtual std::vector<NodeConstPtr> getRawVideoStreams() = 0;
+    std::vector<NodeConstPtr> getRawVideoStreams() {
+        // camera.{video,preview} is linked with XLinkOut
+        const auto& cameras = getCameras();
+        const auto& outputNodes = getRawOutputs();
+        for (const auto& kv : getPipeline().getConnectionMap()) {
+            // filter connections with:
+            // input == {video, preview}
+            // no link with VideoEncoder
+            // link with XLinkOut
+        }
+        return {};
+    }
 
     /**
      * @brief returns the compressed streams exposed by the ROS interface
@@ -46,5 +95,66 @@ struct Pipeline {
      * @brief returns the streams that return a tensor, for interfacing with ROS
      */
     virtual std::vector<NodeConstPtr> getTensorOutStreams() = 0;
+
+    /**
+     * @brief filters the nodes in the pipeline based on requested node types
+     *
+     * @tparam Range an iterable of objects capable of holding a name,
+     * eg: std::vector<std::string> or std::array<std::string>
+     * @param node_names list of names to select
+     * @return std::vector<Pipeline::NodeConstPtr> newly created vector with the requested nodes
+     */
+    template <class Range = std::vector<std::string>>
+    std::vector<NodeConstPtr> filterNodesByName(Range&& node_names) const;
+
+    /**
+     * @brief filters out the nodes in the pipeline based on requested node types
+     *
+     * @tparam Range an iterable of objects capable of holding a name,
+     * eg: std::vector<std::string> or std::array<std::string>
+     * @param node_names list of names to not select
+     * @return std::vector<Pipeline::NodeConstPtr> newly created vector with the requested nodes
+     */
+    template <class Range = std::vector<std::string>>
+    std::vector<NodeConstPtr> filterOutNodesByName(Range&& node_names) const;
 };
+}  // namespace rr
+
+#include <range/v3/view.hpp>
+#include <range/v3/algorithm/find.hpp>
+
+namespace rr {
+namespace rv = ranges::view;
+
+template <class Range>
+std::vector<Pipeline::NodeConstPtr> Pipeline::filterNodesByName(Range&& node_names) const {
+    const auto pipeline = getPipeline();
+    const auto& nodeMap = pipeline.getNodeMap();
+
+    std::vector<NodeConstPtr> filtered_nodes;
+    filtered_nodes.reserve(nodeMap.size() / 2);  // guess of size could be much better
+
+    filtered_nodes =
+            nodeMap | rv::transform([](const auto& kv) { return kv.second; }) | rv::filter([&](const auto& nodePtr) {
+                // return true if name is in th list
+                return ranges::find(node_names, nodePtr->getName()) != ranges::end(node_names);
+            });
+    return filtered_nodes;
+}
+
+template <class Range>
+std::vector<Pipeline::NodeConstPtr> Pipeline::filterOutNodesByName(Range&& node_names) const {
+    const auto pipeline = getPipeline();
+    const auto& nodeMap = pipeline.getNodeMap();
+
+    std::vector<NodeConstPtr> filtered_nodes;
+    filtered_nodes.reserve(nodeMap.size());  // guess of size could be much better
+
+    filtered_nodes =
+            nodeMap | rv::transform([](const auto& kv) { return kv.second; }) | rv::filter([&](const auto& nodePtr) {
+                // return true if name is not in the list
+                return ranges::find(node_names, nodePtr->getName()) == ranges::end(node_names);
+            });
+    return filtered_nodes;
+}
 }  // namespace rr
---
+++ depthai_ros_driver/src/pipeline.cpp
@@ -0,0 +1,5 @@
+#include <range/v3/view.hpp>
+
+#include <depthai_ros_driver/pipeline.hpp>
+
+namespace rr {}  // namespace rr
