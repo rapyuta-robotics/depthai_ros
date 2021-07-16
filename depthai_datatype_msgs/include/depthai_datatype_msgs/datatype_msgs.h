de8ddcf4995d98a72711b3a1427d3d5b6a4ba6c0
Kota Mogami <kota.mogami@rapyuta-robotics.com>
2021-07-16 22:08:26 +0900

    add datatype_msgs.h

---
+++ depthai_datatype_msgs/include/depthai_datatype_msgs/datatype_msgs.h
@@ -0,0 +1,6 @@
+#include <depthai_datatype_msgs/RawImgDetections.h>
+#include <depthai_datatype_msgs/RawTracklets.h>
+#include <depthai_datatype_msgs/RawNNData.h>
+#include <depthai_datatype_msgs/RawImgFrame.h>
+
+#include <depthai_datatype_msgs/RawCameraControl.h>
