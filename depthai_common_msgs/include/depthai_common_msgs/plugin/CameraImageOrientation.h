4b643ca84207ab3a26296bfee8db50b2a7e827e3
Kota Mogami <kota.mogami@rapyuta-robotics.com>
2021-07-19 05:45:06 +0900

    remove empty files

--- depthai_common_msgs/include/depthai_common_msgs/plugin/CameraImageOrientation.h
+++ depthai_common_msgs/include/depthai_common_msgs/plugin/CameraImageOrientation.h
@@ -1,6 +1,3 @@
 #include <depthai-shared/datatype/CameraImageOrientation.hpp>
 
-//#define DEPTHAI_COMMON_MSGS_MESSAGE_CAMERAIMAGEORIENTATION_PLUGIN_CLASS_BODY \
-//    MSGPACK_DEFINE()
-
 MSGPACK_ADD_ENUM(dai::CameraImageOrientation);
