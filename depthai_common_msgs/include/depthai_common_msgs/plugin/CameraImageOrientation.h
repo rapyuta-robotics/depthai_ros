3fb844be88b3fc2544f5ffab37d95a94e2054c7f
Kota Mogami <kota.mogami@rapyuta-robotics.com>
2021-07-19 11:54:53 +0900

    remove empty enum message files from common types

--- depthai_common_msgs/include/depthai_common_msgs/plugin/CameraImageOrientation.h
+++
@@ -1,3 +0,0 @@
-#include <depthai-shared/datatype/CameraImageOrientation.hpp>
-
-MSGPACK_ADD_ENUM(dai::CameraImageOrientation);
--- depthai_common_msgs/include/depthai_common_msgs/plugin/UsbSpeed.h
+++
@@ -1,3 +0,0 @@
-#include <depthai-shared/datatype/UsbSpeed.hpp>
-
-MSGPACK_ADD_ENUM(dai::UsbSpeed);
