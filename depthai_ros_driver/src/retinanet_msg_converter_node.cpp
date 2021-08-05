d89744b4c413f227cd52346923bfa27061dae844
Aditya Ardiya <aditya.ardiya@rapyuta-robotics.com>
2021-08-05 11:35:33 +0900

    backup retinanet_msg_converter_node

---
+++ depthai_ros_driver/src/retinanet_msg_converter_node.cpp
@@ -0,0 +1,178 @@
+#include <ros/ros.h>
+#include <depthai_datatype_msgs/RawNNData.h>
+#include <depthai_datatype_msgs/RawImgDetections.h>
+#include <depthai_datatype_msgs/ImgDetection.h>
+
+#include "depthai-shared/datatype/RawNNData.hpp"
+#include "fp16/fp16.h"  // this thing is from Hunter!
+
+class RetinaNetMsgConverter {
+public:
+    RetinaNetMsgConverter() = default;
+    ~RetinaNetMsgConverter() = default;
+    inline void init() { onInit(); }
+
+private:
+    void onInit() {
+        // nh_ = ros::NodeHandle();
+        pnh_ = ros::NodeHandle("~");
+
+        pub_ = nh_.advertise<depthai_datatype_msgs::RawImgDetections>("raw_det", 10);
+        sub_ = nh_.subscribe("raw_nn", 10, &RetinaNetMsgConverter::__msg__callback__, this);
+
+        if (!pnh_.getParam("confidence_threshold", conf_threshold_)) {
+            pnh_.setParam("confidence_threshold", conf_threshold_);
+        }
+        if (!pnh_.getParam("tensor_name", tensor_name_)) {
+            pnh_.setParam("tensor_name", tensor_name_);
+        }
+    }
+    void __msg__callback__(const depthai_datatype_msgs::RawNNDataConstPtr& msg_in) {
+        pub_msg_.detections.clear();
+
+        auto tensor = getTensor(msg_in, tensor_name_);
+        // test std::vector<uint8_t> instead of std::vector<uint8_t, allocator<void>>
+        std::vector<uint8_t> data_copy(msg_in->data.begin(), msg_in->data.end());
+        auto data = getLayerFp16(data_copy, tensor);
+        float x1, y1, x2, y2;
+        int idx_biggest_conf;
+        float biggest_conf;
+        // data.size() is 84 * 12276 -> 1 dimension
+        // tensor.dim is [84, 12276, 1]
+        // 84 -> x, y, w, h, conf_0 until conf_79
+        // 12276 is the maximum bbox that can be detected
+        const auto dim0 = tensor.dims[0];
+        const auto dim1 = tensor.dims[1];
+        const auto dim2 = tensor.dims[2];
+        const auto n_cls = dim0 - 4;  // 84 - 4 = 80 classes
+        cls_items_.resize(n_cls);
+
+        // 3D idx to 1D idx -> data[h, w, c] to data[h*W*C + w*C + c]
+        for (int i = 0; i < dim1; ++i) {  // loop through all possible bbox
+            x1 = data[i * dim0 + 0];      // data[0 * dim1 + i];
+            y1 = data[i * dim0 + 1];      // data[1 * dim1 + i];
+            x2 = data[i * dim0 + 2];      // data[2 * dim1 + i];
+            y2 = data[i * dim0 + 3];      // data[3 * dim1 + i];
+            if (x1 > x2) std::swap(x1, x2);
+            if (y1 > y2) std::swap(y1, y2);
+
+            if (x1 < 0 || x1 > 1 || y1 < 0 || y1 > 1 || x2 < 0 || x2 > 1 || y2 < 0 || y2 > 1) {
+                // skip if value is outside [0.0, 1.0]
+                continue;
+            }
+
+            float max_cls_items = std::numeric_limits<float>::min();
+            for (int cls = 0; cls < n_cls; ++cls) {
+                cls_items_[cls] = data[i * dim0 + (4 + cls)];  // data[(4 + cls) * dim1  + i];
+                if (cls_items_[cls] > max_cls_items) {
+                    max_cls_items = cls_items_[cls];
+                }
+            }
+            computeBiggestConf(cls_items_, max_cls_items, biggest_conf, idx_biggest_conf);
+            if (biggest_conf < conf_threshold_) {
+                // skip if low confidence
+                continue;
+            }
+            // PUSH data if all good
+            ROS_INFO_STREAM(i << ". cls: " << idx_biggest_conf << " conf: " << biggest_conf << " x1: " << x1
+                              << " y1: " << y1 << " x2: " << x2 << " y2: " << x2);
+            depthai_datatype_msgs::ImgDetection det;
+            det.confidence = biggest_conf;
+            det.label = idx_biggest_conf;
+            det.xmin = x1;
+            det.xmax = x2;
+            det.ymin = y1;
+            det.ymax = y2;
+            pub_msg_.detections.push_back(det);
+        }
+        pub_.publish(pub_msg_);
+    }
+    /**
+     * @brief Get the Tensor given a rawnndata and a the name of the tensor
+     *
+     * @param msg_in
+     * @param tensor_name the name of the tensor
+     *
+     * @return depthai_datatype_msgs::TensorInfo
+     */
+    static depthai_datatype_msgs::TensorInfo getTensor(
+            const depthai_datatype_msgs::RawNNDataConstPtr& msg_in, const std::string& tensor_name) {
+        depthai_datatype_msgs::TensorInfo tensor;
+        for (const auto& t : msg_in->tensors) {
+            if (t.name == tensor_name) {
+                assert(t.dataType == 0 && "t.dataType must be dai::TensorInfo::DataType::FP16");
+                tensor = t;
+            }
+        }
+        return tensor;
+    }
+    /**
+     * @brief Get the Layer as vector of <Floating Point 16> which is 2 bytes
+     *
+     * @param data
+     * @param tensor
+     * @return std::vector<float>
+     */
+    static std::vector<float> getLayerFp16(
+            const std::vector<uint8_t>& data, const depthai_datatype_msgs::TensorInfo& tensor) {
+        // find its offset
+        if (tensor.numDimensions > 0) {
+            std::size_t size = tensor.dims[tensor.numDimensions - 1] * tensor.strides[tensor.numDimensions - 1];
+            std::size_t numElements = size / 2;  // FP16
+
+            std::vector<float> out;
+            out.reserve(numElements);
+            const auto* pFp16Data = reinterpret_cast<const std::uint16_t*>(&data[tensor.offset]);
+            for (std::size_t i = 0; i < numElements; i++) {
+                out.push_back(fp16_ieee_to_fp32_value(pFp16Data[i]));
+            }
+            return out;
+        }
+        return {};
+    }
+    static void computeBiggestConf(
+            std::vector<float>& cls_items, float max_cls_items, float& biggest_conf, int& idx_biggest_conf) {
+        // compute softmax numerator and denominator(which is sum of all numerator)
+        float denominator = 0;
+        for (int i = 0; i < cls_items.size(); ++i) {
+            cls_items[i] = std::exp(cls_items[i] - max_cls_items);  // stable softmax
+            denominator += cls_items[i];
+        }
+        // actual compute of softmax(confidence) = numerator divided by denominator
+        idx_biggest_conf = -1;
+        biggest_conf = std::numeric_limits<float>::min();
+        float curr_conf;
+        for (int i = 0; i < cls_items.size(); ++i) {
+            curr_conf = cls_items[i] / denominator;
+            if (curr_conf > biggest_conf) {
+                biggest_conf = curr_conf;
+                idx_biggest_conf = i;
+            }
+        }
+    }
+    // variables
+    //! @brief store data of the classification value(to be computed using softmax after retrieved by tensor)
+    std::vector<float> cls_items_;
+
+    // params
+    //! @brief only include result that has confidence bigger than conf_threshold_
+    float conf_threshold_ = 0.1;
+    //! @brief final output tensor name
+    std::string tensor_name_ = "StatefulPartitionedCall/functional_17/RetinaNet/concat_2";
+
+    // ros variables
+    ros::Subscriber sub_;
+    ros::Publisher pub_;
+    depthai_datatype_msgs::RawImgDetections pub_msg_;
+    ros::NodeHandle nh_;
+    ros::NodeHandle pnh_;
+};
+
+int main(int argc, char* argv[]) {
+    ros::init(argc, argv, "retinanet_msg_converter_node");
+    RetinaNetMsgConverter node;
+    node.init();
+    ros::spin();
+
+    return 0;
+}
