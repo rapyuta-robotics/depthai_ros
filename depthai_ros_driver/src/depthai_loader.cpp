efeb0e73af8fb8cf25af4b539e316a74d48bfb33
Kunal Tyagi <tyagi.kunal@live.com>
2021-09-16 18:46:45 +0900

    Adding support for multiple devices

--- depthai_ros_driver/src/depthai_loader.cpp
+++ depthai_ros_driver/src/depthai_loader.cpp
@@ -18,13 +18,34 @@ int main(int argc, char** argv) {
     if (!private_nh.getParam("pipeline", pipeline_name)) {
         private_nh.setParam("pipeline", pipeline_name);
     }
+    std::string device_id;
+    dai::DeviceInfo device_info;
+    if (!private_nh.getParam("device_id", device_id)) {
+        ROS_INFO("Trying to find a device");
+        auto info = rr::DeviceROS::getFirstAvailableDevice();
+        device_info = std::get<1>(info);
+        device_id = device_info.getMxId();
+        if (!std::get<0>(info)) {
+            ROS_WARN("Device not available (mxid = %s)", device_id.c_str());
+        }
+        private_nh.setParam("device_id", device_id);
+        ROS_INFO("Found device: %s", device_id.c_str());
+    } else {
+        auto info = rr::DeviceROS::getDeviceByMxId(device_id);
+        device_info = std::get<1>(info);
+        assert(device_id == device_info.getMxId());
+        if (!std::get<0>(info)) {
+            ROS_WARN("Device not available (mxid = %s)", device_id.c_str());
+        }
+        ROS_INFO("Using device: %s", device_id.c_str());
+    }
 
     // load pipeline using pluginlib
     auto plugin = rr::load_pipeline(pipeline_name);
     plugin->configure(private_nh);
     dai::Pipeline p = plugin->getPipeline();
 
-    rr::DeviceROS driver(ros::NodeHandle(ros::this_node::getNamespace()), p.getOpenVINOVersion());
+    rr::DeviceROS driver(ros::NodeHandle(ros::this_node::getNamespace()), p.getOpenVINOVersion(), device_info);
     driver.startPipeline(p);
 
     ros::spin();
