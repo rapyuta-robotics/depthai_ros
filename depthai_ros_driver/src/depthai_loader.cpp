792bb5e92a70585921348c16e5667e0c7d1e0c7a
Kunal Tyagi <tyagi.kunal@live.com>
2021-09-17 14:11:22 +0900

    Changes as requested by Ardiya

--- depthai_ros_driver/src/depthai_loader.cpp
+++ depthai_ros_driver/src/depthai_loader.cpp
@@ -18,26 +18,26 @@ int main(int argc, char** argv) {
     if (!private_nh.getParam("pipeline", pipeline_name)) {
         private_nh.setParam("pipeline", pipeline_name);
     }
-    std::string device_id;
+    std::string mx_id;
     dai::DeviceInfo device_info;
-    if (!private_nh.getParam("device_id", device_id)) {
+    if (!private_nh.getParam("mx_id", mx_id)) {
         ROS_INFO("Trying to find a device");
         auto info = rr::DeviceROS::getFirstAvailableDevice();
         device_info = std::get<1>(info);
-        device_id = device_info.getMxId();
+        mx_id = device_info.getMxId();
         if (!std::get<0>(info)) {
-            ROS_WARN("Device not available (mxid = %s)", device_id.c_str());
+            ROS_WARN("Device not available (mx_id = %s)", mx_id.c_str());
         }
-        private_nh.setParam("device_id", device_id);
-        ROS_INFO("Found device: %s", device_id.c_str());
+        private_nh.setParam("mx_id", mx_id);
+        ROS_INFO("Found device: %s", mx_id.c_str());
     } else {
-        auto info = rr::DeviceROS::getDeviceByMxId(device_id);
+        auto info = rr::DeviceROS::getDeviceByMxId(mx_id);
         device_info = std::get<1>(info);
-        assert(device_id == device_info.getMxId());
+        assert(mx_id == device_info.getMxId());
         if (!std::get<0>(info)) {
-            ROS_WARN("Device not available (mxid = %s)", device_id.c_str());
+            ROS_WARN("Device not available (mx_id = %s)", mx_id.c_str());
         }
-        ROS_INFO("Using device: %s", device_id.c_str());
+        ROS_INFO("Using device: %s", mx_id.c_str());
     }
 
     // load pipeline using pluginlib
@@ -45,7 +45,7 @@ int main(int argc, char** argv) {
     plugin->configure(private_nh);
     dai::Pipeline p = plugin->getPipeline();
 
-    rr::DeviceROS driver(ros::NodeHandle(ros::this_node::getNamespace()), p.getOpenVINOVersion(), device_info);
+    rr::DeviceROS driver(private_nh, p.getOpenVINOVersion(), device_info);
     driver.startPipeline(p);
 
     ros::spin();
