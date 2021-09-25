71c1e3a0d5af591213af20fad06e6009688b2044
Kunal Tyagi <tyagi.kunal@live.com>
2021-09-25 17:51:12 +0900

    Apply Ardiya's renaming and optimization suggestions

--- depthai_ros_driver/include/depthai_ros_driver/converter_and_stamper.hpp
+++ depthai_ros_driver/include/depthai_ros_driver/converter_and_stamper.hpp
@@ -18,19 +18,15 @@ namespace rr {
  * @return std::size_t length of the header
  */
 std::size_t find_header_length(const std::uint8_t* const data) {
-    std::size_t count = 0;
-
     const auto& len = [](auto t) { return ros::serialization::Serializer<decltype(t)>::serializedLength(t); };
 
-    count += len(std::uint32_t{});  // seq
-    count += len(ros::Duration{});  // stamp
+    const static auto pre_frame_len = len(std::uint32_t{}) + len(ros::Duration{});  // seq + stamp
+    const static auto frame_meta_len = len(std::uint32_t{});                        // length of data in frame_id
+    const static auto const_header_len = pre_frame_len + frame_meta_len;
 
-    auto len_frame_id = *(reinterpret_cast<const std::uint32_t*>(data + count));
+    const auto len_frame_id = *(reinterpret_cast<const std::uint32_t*>(data + pre_frame_len));
 
-    count += len(std::uint32_t{});  // length of data in frame_id
-    count += len_frame_id;          // frame_id
-
-    return count;
+    return const_header_len + len_frame_id;
 }
 
 struct FakeStream {
@@ -73,37 +69,39 @@ private:
             }
         };
 
-        int stamped_input_queue_size = 1, unstamped_input_queue_size = 1, output_queue_size = 1;
-        param("stamped_input_queue_size", stamped_input_queue_size);
-        param("unstamped_input_queue_size", unstamped_input_queue_size);
-        param("output_queue_size", output_queue_size);
+        int stamp_source_queue_size = 1, unstamped_queue_size = 1, stamped_queue_size = 1;
+        param("stamp_source_queue_size", stamp_source_queue_size);
+        param("unstamped_queue_size", unstamped_queue_size);
+        param("stamped_queue_size", stamped_queue_size);
 
         auto nh = Node::getNodeHandle();
-        sub_stamped_ = nh.subscribe("stamped", stamped_input_queue_size, &ConverterAndStamper::stamped_callback, this);
+        sub_stamp_source_ = nh.subscribe(
+                "stamp_source", stamp_source_queue_size, &ConverterAndStamper::stamp_source_callback, this);
         sub_unstamped_ =
-                nh.subscribe("unstamped", unstamped_input_queue_size, &ConverterAndStamper::unstamped_callback, this);
+                nh.subscribe("unstamped", unstamped_queue_size, &ConverterAndStamper::unstamped_callback, this);
 
         topic_tools::ShapeShifter shifter;
         namespace mt = ros::message_traits;
+        // @TODO: this can be made parameter or ctor input so that the class is not templated on MessageStamped
         shifter.morph(
                 mt::md5sum<MessageStamped>(), mt::datatype<MessageStamped>(), mt::definition<MessageStamped>(), "");
-        pub_stamped_ = shifter.advertise(nh, "output", output_queue_size, false);
+        pub_stamped_ = shifter.advertise(nh, "stamped", stamped_queue_size, false);
     }
 
-    void stamped_callback(const ShapeShifterConstPtr& stamped_message) {
-        FakeStream stream(stamped_buffer_);
-        stamped_message->write(stream);
+    void stamp_source_callback(const ShapeShifterConstPtr& stamp_source_msg) {
+        FakeStream stream(stamp_source_buffer_);
+        stamp_source_msg->write(stream);
     }
 
-    void unstamped_callback(const ShapeShifterConstPtr& unstamped_message) {
-        if (stamped_buffer_.empty()) {
+    void unstamped_callback(const ShapeShifterConstPtr& unstamped_msg) {
+        if (stamp_source_buffer_.empty()) {
             return;
         }
-        const auto header_len = find_header_length(stamped_buffer_.data());
+        const auto header_len = find_header_length(stamp_source_buffer_.data());
 
-        const auto total_len = unstamped_message->size() + header_len;
+        const auto total_len = unstamped_msg->size() + header_len;
         unstamped_buffer_.resize(total_len);
-        std::copy(stamped_buffer_.cbegin(), stamped_buffer_.cbegin() + header_len, unstamped_buffer_.begin());
+        std::copy(stamp_source_buffer_.cbegin(), stamp_source_buffer_.cbegin() + header_len, unstamped_buffer_.begin());
 
         FakeStream stream(unstamped_buffer_, header_len);
 
@@ -114,7 +112,7 @@ private:
         * copy both together to incoming message
         * morph the message
         */
-        unstamped_message->write(stream);  // move the actual data at the end of the stream
+        unstamped_msg->write(stream);  // move the actual data at the end of the stream
 
         auto msg_stamped = boost::make_shared<topic_tools::ShapeShifter>();  // for 0 copy
         msg_stamped->read(stream);  // if only ShapeShifter allowed simple insertion into the internal msg buffer
@@ -125,11 +123,11 @@ private:
         pub_stamped_.publish(msg_stamped);
     }
 
-    std::vector<std::uint8_t> stamped_buffer_;
+    std::vector<std::uint8_t> stamp_source_buffer_;
     std::vector<std::uint8_t> unstamped_buffer_;
 
     ros::Publisher pub_stamped_;
-    ros::Subscriber sub_stamped_;
+    ros::Subscriber sub_stamp_source_;
     ros::Subscriber sub_unstamped_;
 };
 }  // namespace rr
